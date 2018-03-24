97faad13cb68679c7cbaa5d4ec503e0d330514b3
GeloPC <michaelangelosdr@gmail.com>
2018-03-24 23:13:32 +0800

    Added Event triggers

---
+++ Word_Search_Game/Assets/Mouse_Raycaster.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Mouse_Raycaster : MonoBehaviour, IPointerDownHandler {
+
+	Ray ray;
+	// Use this for initialization
+	void Start () {
+		ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+	public void OnPointerDown(PointerEventData eventData)
+	{
+		Debug.Log ("Clicked: " + gameObject.name);
+	}
+}
---
+++ Word_Search_Game/Assets/Tile_Script.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Tile_Script : MonoBehaviour {
+
+	//
+	Tile_States tile_state;
+	[SerializeField] Image Tile_BG_Image;
+
+	[SerializeField] GameObject Tile_BG_Image_Idle;
+	[SerializeField] GameObject Tile_BG_Image_Selected;
+	[SerializeField] Text Tile_Text;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (tile_state == Tile_States.Idle_State) {
+			set_this_Tile_to (Tile_BG_Image_Idle);
+		} else if (tile_state == Tile_States.Selected_State) {
+			set_this_Tile_to (Tile_BG_Image_Selected);
+		}
+
+	}
+
+	public void set_this_Tile_to(GameObject setobject)
+	{
+		Tile_BG_Image_Idle.SetActive (false);
+		Tile_BG_Image_Selected.SetActive (false);
+		setobject.SetActive (true);
+
+	}
+
+	public void Selected()
+	{
+		tile_state = Tile_States.Selected_State;
+	}
+	public void Unselected()
+	{
+		tile_state = Tile_States.Idle_State;
+	}
+
+}
+public enum Tile_States
+{
+	Idle_State,
+	Selected_State,
+	Completed_State
+}
